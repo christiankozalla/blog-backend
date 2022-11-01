7700b82df6eab8a3c92911373590962cbab27bb9
Christian Kozalla <christian.kozalla@gmail.com>
2022-11-01 15:10:03 +0100

    feat: expose db-client

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { decrypt, encrypt } from "./hash";
 import { authenticationHandler } from "./auth";
 import { initBlogFileSystem } from "./init-blog-files";
 import { initAuthFileSystem } from "./init-auth-files";
+import * as dbClient from "./db-client";
 import * as helpers from "./helpers";
 
 export {
   authenticationHandler,
+  dbClient,
   decrypt,
   encrypt,
   helpers,
