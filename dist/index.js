6608912e65dbc1f4c6a557a0cf80c4bac1b87dea
Christian Kozalla <christian.kozalla@gmail.com>
2022-11-01 15:12:38 +0100

    chore: release db-client

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,6 @@ import { decrypt, encrypt } from "./hash";
 import { authenticationHandler } from "./auth";
 import { initBlogFileSystem } from "./init-blog-files";
 import { initAuthFileSystem } from "./init-auth-files";
+import * as dbClient from "./db-client";
 import * as helpers from "./helpers";
-export { authenticationHandler, decrypt, encrypt, helpers, initAuthFileSystem, initBlogFileSystem, };
+export { authenticationHandler, dbClient, decrypt, encrypt, helpers, initAuthFileSystem, initBlogFileSystem, };
