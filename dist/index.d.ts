b37b53bfab93416d7847c60e03190d2bf8b56342
Christian Kozalla <christian.kozalla@gmail.com>
2022-11-01 15:26:33 +0100

    fix: expose explicit types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,4 +4,6 @@ import { initBlogFileSystem } from "./init-blog-files";
 import { initAuthFileSystem } from "./init-auth-files";
 import * as dbClient from "./db-client";
 import * as helpers from "./helpers";
+import type { Frontmatter, Post, Response, User } from "./types";
 export { authenticationHandler, dbClient, decrypt, encrypt, helpers, initAuthFileSystem, initBlogFileSystem, };
+export type { Frontmatter, Post, Response, User };
